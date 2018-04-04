ae6dace06284d568b97d69e397fd13e81796cc51
ksyar2001 <love.power.yeah@gmail.com>
2018-04-04 02:12:06 -0500

    add cors and server rest api

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ var express = require('express');
 var nodeMailer = require('nodemailer');
 var bodyParser = require('body-parser');
 var app = express();
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//cors header for cross origin requests
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+	next();
+});
 
 app.post('/sendEmail', (req, res) => {
 	return res.send('ok');
 });
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
